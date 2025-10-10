382cdc45f8e75e7e133c717a03dc0c569cf95a2e
Suzune <124695777+Suzune705@users.noreply.github.com>
2025-10-10 07:09:44 +0700

    chore: update library

--- src/java/com/library/model/user.java
+++ src/java/com/library/model/user.java
@@ -4,11 +4,22 @@
  */
 package com.library.model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  *
  * @author laptop gigabyte
  */
 public class user {
-    private String name ;
-    private String name2 ;
+
+    private static final Logger logger = LoggerFactory.getLogger(user.class);
+
+    public static void main(String[] args) {
+        logger.trace("TRACE: Logger setup OK!");
+        logger.debug("DEBUG: This is a debug message.");
+        logger.info("INFO: Hello from SLF4J + Logback!");
+        logger.warn("WARN: Something might be wrong...");
+        logger.error("ERROR: Something went wrong!");
+    }
 }
