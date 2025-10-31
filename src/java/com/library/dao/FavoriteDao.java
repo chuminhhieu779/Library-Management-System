4e96a81bafa28ee2aeb9923aad19af168a6d5cf6
Suzune <124695777+Suzune705@users.noreply.github.com>
2025-10-31 20:38:20 +0700

    feactor(admin): remove book

---
+++ src/java/com/library/dao/FavoriteDao.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.library.dao;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author hieuchu
+ */
+public interface FavoriteDao {
+    void removeBookByID(Connection conn , int bookID);
+}
