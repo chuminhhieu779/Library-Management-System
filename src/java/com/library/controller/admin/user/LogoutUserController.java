f644224d0df8e0f579f97ec093c28ce48ba9c2db
Suzune <124695777+Suzune705@users.noreply.github.com>
2025-11-06 10:34:24 +0700

    fix: using CI for test

--- src/java/com/library/controller/admin/user/LogoutUserController.java
+++ src/java/com/library/controller/admin/user/LogoutUserController.java
@@ -19,7 +19,6 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import org.apache.catalina.Session;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
