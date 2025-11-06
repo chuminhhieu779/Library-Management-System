5cc2223c85f08d8caf781bc9cc167c1f3bec566d
Suzune <124695777+Suzune705@users.noreply.github.com>
2025-11-06 23:49:34 +0700

    refactor: use exception for login

---
+++ src/java/com/library/exception/UserNotFoundException.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.library.exception;
+
+/**
+ *
+ * @author hieuchu
+ */
+public class UserNotFoundException extends LibraryException{
+    
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+    
+}
