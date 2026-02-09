3e14d5d5a41d4505507519cfa49ba212ae7d9a0c
Suzune <124695777+Suzune705@users.noreply.github.com>
2026-02-09 23:17:08 +0700

    update

--- src/java/com/library/controller/borrowing/BorrowBookController.java
+++ src/java/com/library/controller/borrowing/BorrowBookController.java
@@ -4,16 +4,10 @@
  */
 package com.library.controller.borrowing;
 
-import com.library.dao.BookDao;
-import com.library.dao.BorrowingDao;
-import com.library.factory.DaoFactory;
-import com.library.dao.UserDao;
-import com.library.dao.UserDaoImpl;
 import com.library.factory.ServiceFactory;
 import com.library.service.ActivityService;
 import com.library.service.BorrowingService;
 import java.io.IOException;
-import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -28,39 +22,48 @@ import jakarta.servlet.http.HttpSession;
 @WebServlet(name = "BorrowBook", urlPatterns = {"/borrowing/borrow"})
 public class BorrowBookController extends HttpServlet {
 
-    private final BorrowingService borrowService = ServiceFactory.getBorrowService();
-    private final ActivityService activityService = ServiceFactory.getActivityService();
+    private final BorrowingService borrowService;
+    private final ActivityService activityService;
+
+    public BorrowBookController(BorrowingService borrowService, ActivityService activityService) {
+        this.borrowService = borrowService;
+        this.activityService = activityService;
+    }
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
+      
         HttpSession session = request.getSession(false);
-        if (session == null || session.getAttribute("account") == null) {
-            session.setAttribute("error", "you have to login before using our service!");
+        if (session == null) {
+//book.list           
             response.sendRedirect(request.getContextPath() + "/book/detail");
             return;
         }
+// book/list
+        if (session.getAttribute("account") == null) {
+            session.setAttribute("success", "you have to login before using our service!");
+            response.sendRedirect(request.getContextPath() + "/book/detail"); 
+            return;
+        }
+// slug       
         String slug = request.getParameter("slug");
         int bookID = Integer.parseInt(request.getParameter("bookID"));
-        String account = (String) session.getAttribute("account");
+// account        
+        String account = (String) session.getAttribute("accout");
 
+        
         int userID = borrowService.getUserIDByAccount(account);
 
         if (borrowService.canBorrowBook(bookID, userID)) {
             borrowService.borrowBook(slug, bookID, userID);
             activityService.BookActivityOfUser(account, 3, bookID);
-            session.setAttribute("success", "borrow book request has sent  !!");
+            session.setAttribute("success", "borrow book request has sent!!");
         } else {
+            // fix :    session.setAttribute("failed", "you already borrowed this book !!!");
             session.setAttribute("failed", "you already borrowed this book !!!");
         }
         response.sendRedirect(request.getContextPath() + "/book/detail?slug=" + slug + "&bookID=" + bookID);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
     }
 
 }
