5c1fb2159e33d1f5137ba900e9f3354e7993b1e5
Suzune <124695777+Suzune705@users.noreply.github.com>
2026-02-11 23:14:24 +0700

    update

--- src/java/com/library/controller/borrowing/ReturnedBooksListController.java
+++ src/java/com/library/controller/borrowing/ReturnedBooksListController.java
@@ -7,16 +7,13 @@ package com.library.controller.borrowing;
 
 import com.library.dao.BorrowingDao;
 import com.library.dao.BorrowingDaoImpl;
-import com.library.model.entity.Book;
 import java.io.IOException;
-import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import java.util.List;
 import java.util.Map;
 
 /**
